7c860df3fca266cb2920d3cbcc21d9ee880e1292
Jess <jtrinh619@gmail.com>
2021-02-02 13:32:22 -0800

    [add] refactoring-pep8 activities

---
+++ refactoring-pep8/whitespace_naming_convention2.py
@@ -0,0 +1,34 @@
+# PEP8 - whitespaces and variable names.
+class pizza:
+    def __init__(cls, obj, mybread_type, cheese_type, meat_type, pizza_toppings, size):
+        obj.bread_type = mybread_type
+        obj.cheese_type = cheese_type
+        obj.meatType = meat_type
+        obj.toppings = pizza_toppings
+        obj.size = size       
+
+    @classmethod
+    def create_chicago_pizza(cls, size):
+        bread = 'deep-dish bread'
+        cheese = 'mozzarella cheese'
+        meat_type = 'Italian sausage'
+        toppings = ['green bell pepper', 'mushroom', 'chunky tomato sauce', 'onion']
+        return cls(bread, cheese, meat_type, toppings, size)    
+
+    @classmethod
+    def create_california_pizza(cls, meat_type, size):
+        bread = 'thin crust'
+        cheese = 'feta cheese'
+        toppings = ['garlic', 'spinach', 'broccoli', 'olives', 'red onion', 'red bell pepper']
+        return cls(bread, cheese, meat_type, toppings, size) 
+
+    def printInfo(obj):
+        print('bread type is: ', obj.bread_type)
+        print('cheese type is: ', obj.cheese_type)
+        print('meat type is: ', obj.meat_type)
+        print('Toppings are: ', end='')
+        print(', '.join(map(str, obj.toppings)))
+
+    
+myPizza = pizza.create_california_pizza('chicken', 'large')
+myPizza.printInfo()
