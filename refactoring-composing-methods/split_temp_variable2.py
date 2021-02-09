5b6bd60008f59db1e06325e33bdd204dd39bf9cb
Jess <jtrinh619@gmail.com>
2021-02-09 11:01:47 -0800

    [refactor] variables in code

--- refactoring-composing-methods/split_temp_variable2.py
+++ refactoring-composing-methods/split_temp_variable2.py
@@ -4,8 +4,8 @@ def save_into_db(info):
     print("saved into databse")
 
 
-user_input = input('Please enter your username: ')
-save_into_db(user_input)
-user_input = int(input('Please enter your birth year: '))
-age = 2020 - user_input
-print("You are",age, "years old.")
+username_input = input('Please enter your username: ')
+save_into_db(username_input)
+birthyear_input = int(input('Please enter your birth year: '))
+age = 2020 - birthyear_input
+print("You are ", age, " years old.")
