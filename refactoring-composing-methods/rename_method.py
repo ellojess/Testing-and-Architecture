74854d4b0d9c5a57f816145daef879132b90030a
Jess <jtrinh619@gmail.com>
2021-02-09 12:02:37 -0800

    [refactor] composing methods, updating var names and methods

--- refactoring-composing-methods/rename_method.py
+++ refactoring-composing-methods/rename_method.py
@@ -1,27 +1,27 @@
 # Rename Method
 # Reference: https://parade.com/1039985/marynliles/pick-up-lines/
 
-def cal_un_gr(graph):   # TODO: Rename this function to reflect what it's doing.
+def calculate_area_graph(graph):   # TODO: Rename this function to reflect what it's doing.
     """Calculate the area under the input graph."""
     # bla bla bla.
     pass
 
 #####################
 
-def get_value(li):  # TODO: Rename this function to reflect what it's doing.
-    m = li[0]
+def get_max_value(li):  # TODO: Rename this function to reflect what it's doing.
+    max_value = li[0]
     for value in li:
-        if value > m:
-            m = value
-    return m
+        if value > max_value:
+            max_value = value
+    return max_value
 
 
 li = [5, -1, 43, 32, 87, -100]
-print(get_value(li))
+print(get_max_value(li))
 
 ############################
-def process(sentence):  # TODO: Rename this function to reflect what it's doing.
+def get_words_in_sentence(sentence):  # TODO: Rename this function to reflect what it's doing.
     words = sentence[0:].split(' ')
     return words
 
-print(process('If you were a vegetable, you’d be a ‘cute-cumber.'))
+print(get_words_in_sentence('If you were a vegetable, you’d be a ‘cute-cumber.'))
