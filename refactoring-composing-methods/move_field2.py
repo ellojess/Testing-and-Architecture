7b88b2a7a3bc25a91f34a5ebf0ab934b9af868ed
Jess <jtrinh619@gmail.com>
2021-02-16 12:20:41 -0800

    [refactor] using organizing data refactoring methods

--- refactoring-composing-methods/move_field2.py
+++ refactoring-composing-methods/move_field2.py
@@ -1,13 +1,13 @@
 # Move Field
 
 class Car:
-    def __init__(self, engine, wheels, cabin, tpms_di, fuel_tank):
+    def __init__(self, engine, wheels, cabin, fuel_tank):
         self.engine = engine
-        # TODO: tpms is better to be in the Wheel class. 
+        # TODO: tpms is better to be in the Wheel class. ✅
         # Each wheel has a single tpms attached to it. 
         # Thus, instead of having a list of tpms in 'Car' class
         # have each of the tpms in each 'Wheel'.
-        self.tpms_list = tpms_di  # Tire Pressure Monitoring System.
+        # self.tpms_list = tpms_di  # Tire Pressure Monitoring System.
         self.wheels = wheels
         # Set wheels' car reference into each wheel.
         for w in wheels:
@@ -18,13 +18,14 @@ class Car:
 
     
 class Wheel:
-    # TODO: You may add tpms as a method parameter here to 
+    # TODO: You may add tpms as a method parameter here to ✅
     #       initilaize the 'Wheel' object or you can create
     #       a setter method to set the tpms of the wheel. (you can do 
     #       both of course.)
-    def __init__(self, car = None, wheel_location = None):
+    def __init__(self, car = None, wheel_location = None, tpms_di = None):
         self.car = car
         self.wheel_location = wheel_location
+        self.tpms_list = tpms_di  # Tire Pressure Monitoring System.
 
     def install_tire(self):
         print('remove old tube.')
@@ -72,7 +73,7 @@ class Cabin:
     
 
 engine = Engine()
-# TODO: Rewrite the following after moving tpms to the 'Wheel' class.
+# TODO: Rewrite the following after moving tpms to the 'Wheel' class. ✅
 wheels = [Wheel(None, 'front-right'), Wheel(None, 'front-left'), 
           Wheel(None, 'back-right'), Wheel(None, 'back-left')]
 
@@ -83,4 +84,4 @@ tpms_di = {'front-right': Tpms(983408543), 'front-left':Tpms(4343083),
 
 fuel_tank = FuelTank()
 
-my_car = Car(engine, wheels, cabin, tpms_di, fuel_tank)
+my_car = Car(engine, wheels, cabin, fuel_tank)
--- refactoring-composing-methods/replace_magic_numbers_with_named_constants.py
+++ refactoring-composing-methods/replace_magic_numbers_with_named_constants.py
@@ -5,7 +5,8 @@
 q1 = int(input('Enter a value of charge q1: '))
 q2 = int(input('Enter a value of charge q2: '))
 distance = int(input("Enter the distance be10tween two charges: "))
-force = 8.9875517923*1e9 * q1 * q2/(distance**2)
+coulombs_constand = 8.9875517923*1e9
+force = coulombs_constand * q1 * q2/(distance**2)
 print ("Electric Force between q1 and q2 is: ", force, "Newton")
 # Second Section
 num = int(input('Enter an integer number: '))
