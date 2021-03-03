15a7e7a07ade326ebe7b11a84ff8c963f989f8d8
Jess <jtrinh619@gmail.com>
2021-03-03 14:10:02 -0800

    [refactor] replace temp with query fowler

--- refactoring-composing-methods/replace_temp_with_query_fowler.py
+++ refactoring-composing-methods/replace_temp_with_query_fowler.py
@@ -1,12 +1,14 @@
-  # Adapted from a Java code in the "Refactoring" book by Martin Fowler.
+# Adapted from a Java code in the "Refactoring" book by Martin Fowler.
 # Replace temp with query
 # Code snippet. Not runnable.
-def get_price():
-    base_price = quantity * item_price
+def get_price(quantity, item_price):
     discount_factor = 0
-    if base_price > 1000: 
+    if base_price(quantity, item_price) > 1000: 
         discount_factor = 0.95
     else: 
         discount_factor = 0.98
-    return base_price * discount_factor
+    return base_price(quantity, item_price) * discount_factor
+
+def base_price(quantity, item_price):
+    return quantity * item_price
     
